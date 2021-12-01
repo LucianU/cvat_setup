7277acd6dfa68a624fc6801ebf77cc325c159179
Lucian Ursu <lucian.ursu@gmail.com>
2021-12-01 16:42:20 +0200

    Fix and implement stuff

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 # Required arguments: HOST, USER, SSH PRIVATE KEY
+if [ "$#" -ne 3 ];
+then
+  echo "3 arguments required: HOST, USER, SSH PRIVATE KEY"
+  echo "$# provided"
+  exit 1
+fi
 
 # Install docker on the target host
-ansible-playbook ansible/deploy \
+./env/bin/ansible-playbook ansible/deploy.yml \
   --inventory="$1," \
   --user="$2" \
-  --private-key="$3"
-
-# Clone the CVAT project
-git clone https://github.com/openvinotoolkit/cvat /tmp/
-
-# Create the Docker context for the target host
-docker context create cvat-host --docker host="ssh://$2@$1"
+  --private-key="$3" \
+  -e user=$2
 
 # Deploy the app to the target host
+echo "Deploying CVAT to remote host"
 docker-compose --context cvat-host -f /tmp/cvat/docker-compose.yml up -d
--- setup.sh
+++ setup.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
 
-python3 -m venv env
+if [ "$#" -ne 2 ]
+then
+  echo "2 arguments required: HOST, USER"
+  echo "$# provided"
+  exit 1
+fi
 
+PYTHON_ENV_DIR="env"
+CVAT_REPO_URL="https://github.com/openvinotoolkit/cvat"
+CLONE_TARGET_PATH="/tmp/cvat"
+
+if [ ! -d env ]
+then
+  echo "Creating local virtual env..."
+  python3 -m venv env
+fi
+
+echo "Installing Python libraries..."
 ./env/bin/pip install -r pip-requirements.txt
 
 hash docker-compose 2>/dev/null || echo "Please install docker-compose."
+
+# Clone the CVAT repo
+if [ ! -d "${CLONE_TARGET_PATH}" ]
+then
+  echo "Cloning CVAT repo..."
+  git clone "${CVAT_REPO_URL}" /tmp/cvat
+else
+  echo "CVAT has already been cloned."
+fi
+
+# Create the Docker context for the target host
+echo "Creating Docker Context for target host..."
+docker context create cvat-host --docker host="ssh://$2@$1"
